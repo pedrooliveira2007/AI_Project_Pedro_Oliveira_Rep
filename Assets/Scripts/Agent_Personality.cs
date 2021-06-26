b12840fd843288f984010ec78aecfc2e2bc89b70
Pedro Oliveira <pedrooliveira2007@hotmail.com>
2021-06-26 23:19:27 +0100

    personality script added

---
+++ Assets/Scripts/Agent_Personality.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Personality", menuName = "ScriptableObjects/Personality", order = 1)]
+public class Agent_Personality : ScriptableObject
+{
+    public float moveSpeed = 100;
+    public float maxHunger = 100;
+    public float minHunger = 90;
+    public float eatRate = 1;
+    public float hungerRate = 1;
+    public float maxFatigue = 100;
+    public float minFatigue = 90;
+    public Material myMaterial;
+}
